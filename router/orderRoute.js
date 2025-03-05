1bb04667fc9736a0b0d1fedc26a83f08515eb887
Your Name <your_email@example.com>
2025-03-05 19:47:11 +0530

    fix payment apis d

--- router/orderRoute.js
+++ router/orderRoute.js
@@ -1,6 +1,5 @@
-import { create } from "domain";
-import { createOrder } from "../Controllers/order.controller";
 import express from 'express'
+import { createOrder } from "../Controllers/order.controller.js";
 const orderRouter = express.Router()
 
 orderRouter.post('/makeOrder',createOrder)
