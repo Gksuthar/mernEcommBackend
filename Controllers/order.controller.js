1b73d4c60d307f2376adf22b18a35f1423ea4b24
Your Name <your_email@example.com>
2025-03-05 16:56:30 +0530

    fix payment

--- Controllers/order.controller.js
+++ Controllers/order.controller.js
@@ -1,5 +1,10 @@
 
 // app.post("/create-order", async (req, res) => {
+
+  const razorpayInstance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
     export const createOrder=async(req,res)=>{
     try {
       const { amount } = req.body;
